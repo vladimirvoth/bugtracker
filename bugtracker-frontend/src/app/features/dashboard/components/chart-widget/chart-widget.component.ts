0410c82aa89e40d809bf9d947c5d2e1e50ec66ae
Vlad <thecodervlad@gmail.com>
2021-01-05 20:14:52 +0100

    Create chart widget

--- bugtracker-frontend/src/app/features/dashboard/components/chart-widget/chart-widget.component.ts
+++ bugtracker-frontend/src/app/features/dashboard/components/chart-widget/chart-widget.component.ts
@@ -1,3 +1,6 @@
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label } from 'ng2-charts';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart-widget.component.scss']
 })
 export class ChartWidgetComponent implements OnInit {
+  pieChartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      position: 'top'
+    }
+  };
+  pieChartLabels: Label[] = [
+    ['Open'],
+    ['In Progress'],
+    ['Testing'],
+    ['Closed']
+  ];
+  pieChartData: number[] = [];
+  pieChartType: ChartType = 'pie';
+  pieChartLegend = true;
+  pieChartColors = [
+    {
+      backgroundColor: [
+        'rgba(255,0,0,0.3)',
+        'rgba(0,255,0,0.3)',
+        'rgba(0,0,255,0.3)',
+        'yellow'
+      ]
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.pieChartData = [1, 2, 2, 1];
   }
-
 }
--- bugtracker-frontend/src/app/features/dashboard/dashboard.module.ts
+++ bugtracker-frontend/src/app/features/dashboard/dashboard.module.ts
@@ -1,11 +1,13 @@
+import { ChartsModule } from 'ng2-charts';
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicketsModule } from '@features/tickets/tickets.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { DashboardComponent } from './containers/dashboard.component';
 import { ChartWidgetComponent } from './components/chart-widget/chart-widget.component';
+import { DashboardComponent } from './containers/dashboard.component';
 
 const routes: Routes = [
   {
@@ -20,7 +22,8 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forChild(routes),
     FontAwesomeModule,
-    TicketsModule
+    TicketsModule,
+    ChartsModule
   ]
 })
 export class DashboardModule {}
